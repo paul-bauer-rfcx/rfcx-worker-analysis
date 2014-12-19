509d9b390af08f9eacf40b04fce086f4d6ef86e7
Kevin Keller <flygeneticist@gmail.com>
2014-12-19 16:09:49 +0100

    Cleaned up older parts of code.

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ from tornado.options import options
 
 # import RFCx custom modules
 from modules import feature_extraction # feature extraction with Welch
-# from modules import known_recognition # compare against known sound profiles
-# from modules import anomaly_detection # look for anomalies in sound profile
+# from modules import sound_profiling # compare against known / anomaly sound profiles
 # from modules import alert_ml # trigger alerts and save spectrum for ML
 
 class MainPage(tornado.web.RequestHandler):
@@ -25,21 +24,21 @@ class MainPage(tornado.web.RequestHandler):
 class FeatureExtraction(tornado.web.RequestHandler):
   @tornado.web.asynchronous
   @tornado.gen.engine
-  def get(self):
+  def post(self):
     # grab wav file from S3 location based on filename in JSON data pushed from SQS with Boto
-    conn = boto.connect_s3(options.AWS_KEY, options.AWS_SECRET)
-    bucket = conn.get_bucket(options.BUCKET_NAME)
     filename = json.loads(self.request.body)['filename']
-    key = bucket.get_key(filename)
-    wav_file = key.get_contents_to_filename('sound.wav')
+    # conn = boto.connect_s3(options.AWS_KEY, options.AWS_SECRET)
+    # bucket = conn.get_bucket(options.BUCKET_NAME)
+    # key = bucket.get_key(filename)
+    # wav_file = key.get_contents_to_filename('sound.wav')
     
     # process wav file with feature extraction module
     client = tornado.httpclient.AsyncHTTPClient()
     analyzer = feature_extraction.FeatureExtractor()
-    response = yield tornado.gen.Task(client.fetch, analyzer.extract(wav_file))
+    response = yield tornado.gen.Task(client.fetch, analyzer.extract(filename))
     spectrum = response.effective_url
 
-    # test only! Rewrite to send numpy data to perm storage(redis?)
+    # Only for testing spectrum object returned from call! REMOVE!
     self.write("""<h2>Finshed processing audio! Frequency spectrum has been created!</h2>
                   <ul>
                     <li>Length: %s </li>
@@ -48,8 +47,8 @@ class FeatureExtraction(tornado.web.RequestHandler):
               """ % (len(spectrum), sys.getsizeof(spectrum)))
     self.finish()
 
-# TO DO: pass spectrum data array to the Sound Profiler modules
 class SoundProfiling(tornado.web.RequestHandler):
+  # TO DO: pass spectrum data array to the Sound Profiler module
   @tornado.web.asynchronous
   @tornado.gen.engine
   def get(self):
@@ -59,19 +58,22 @@ class SoundProfiling(tornado.web.RequestHandler):
     profile = response.effective_url
     self.finsh()
 
-# class AnomalyDetection(tornado.web.RequestHandler):
-#   @tornado.web.asynchronous
-#   @tornado.gen.engine
-#     client = tornado.httpclient.AsyncHTTPClient()
-#     profiler = known_sounds_profiler.SoundProfiler()
-#     response = yield tornado.gen.Task(client.fetch, profiler.extract(redis_key))
-#     profile = response.effective_url
-#     self.finsh()
-
-# TO DO:  1) Trigger 3rd party alert via API call
-#         2) Send sprectum data to ML DB for processing
-#         3) Final cleanup of sound file
 class AlertML(tornado.web.RequestHandler):
+  # TO DO:  1) Trigger 3rd party alert via API call
+  #         2) Send sprectum data to ML DB for processing
+  #         3) Final cleanup of sound file
+  # Break up into sperate classes to handle ML pushing sound profiles back to analyzer? 
+  @tornado.web.asynchronous
+  @tornado.gen.engine
+  def get(self):
+    client = tornado.httpclient.AsyncHTTPClient()
+    profiler = known_sounds_profiler.SoundProfiler()
+    response = yield tornado.gen.Task(client.fetch, profiler.extract(redis_key))
+    profile = response.effective_url
+    self.finsh()
+
+class AddKnownSoundProfile(tornado.web.RequestHandler):
+  # TO DO: allow the ML framework a way to interact with sound profiling to update it as needed
   @tornado.web.asynchronous
   @tornado.gen.engine
   def get(self):
@@ -84,11 +86,10 @@ class AlertML(tornado.web.RequestHandler):
 
 # routes to call the correct handlers
 app = tornado.web.Application([
-  (r"/", MainPage), # testing placeholder! Remove!
   (r"/featureExtraction", FeatureExtraction),
   (r"/soundProfiling", SoundProfiling),
-  # (r"/anomalyDetection", AnomalyDetection)
-  (r"/AlertML", AlertML)
+  (r"/alertML", AlertML),
+  (r"/addKnownSoundProfile", AddKnownSoundProfile)
 ])
 
 
@@ -97,6 +98,5 @@ if __name__ == "__main__":
   base = "http://localhost:" + str(port) + "/"
   app.listen(port)
   print "Web server started: " + base
-  print "Test FeatExtr: " + base + "featureExtraction?q=test.wav"
   tornado.ioloop.IOLoop.instance().start()
   
