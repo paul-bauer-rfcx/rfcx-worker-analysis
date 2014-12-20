00e1f05a674e1dcf6376ae5fd610609d2b5d2f6b
Kevin Keller <flygeneticist@gmail.com>
2014-12-20 18:34:07 +0100

    Added UpdateSoundProfile class and removed Spectrum class from SL.

--- modules/service_layer.py
+++ modules/service_layer.py
@@ -3,17 +3,17 @@ from modules.domain_modules import sound_profiling # compare against known / ano
 from modules.domain_modules import alerts # trigger alerts
 from modules.domain_modules import machine_learning  # save spectrum for ML
 
-# TO DO: Define stub classes/objects for the interfaces in service layer
-
-class SpectralAnalysis:
-  def __init__(self):
+class AnalyzeSound:
+  def __init__(self, key):
     '''Spectral Analysis Class:
     Processes audio files and outputs spectrum data in the form of a Spectrum Class object.
     '''
+    self.key = validate(key)
     pass
 
-  def validate(self):
-    pass
+  def validate(key):
+    # check JSON string against signed S3 url regex
+    return re.match(key)
 
   def analyze(self):
     '''Processes a wav file into a Spectrum class object.'''
@@ -21,12 +21,15 @@ class SpectralAnalysis:
     # return Spectrum()
 
 
-class Spectrum():
-  def __init__(self):
-    '''Spectrum Class: 
-    Holds all data from the transformation of an audio file into it's 
-    amplitude(dB) over frequency(Hz) values.'''
+class UpdateSoundProfile:
+  def __init__(self, profile):
+      '''Update Sound Profile Class: Updates or creates sound profile based on ML framework.'''
+      pass
+
+  def validate(profile):
+    # check JSON data against valid profile object properties
+    # return re.match(profile)
     pass
 
-  def validate(self):
+  def update(self):
     pass
