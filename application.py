6ec813b3b096a37ad869348509126c89ae62c575
Kevin Keller <flygeneticist@gmail.com>
2014-12-28 16:12:45 +0100

    Test AWS deployment with basic hello world app.

--- application.py
+++ application.py
@@ -1,35 +1,41 @@
 # web server modules
-from flask import Flask, request
-import gevent
-import requests
-import json
+from flask import flask, request
+# import gevent
+# import requests
+# import json
 
 # import RFCx custom modules via service layer
-from modules import service_layer
+# from modules import service_layer
 
 # setup web app instance
 application = Flask(__name__)
 
-# routes to call the correct request handlers
-@application.route('/analyzeSound', methods=['POST'])
-def analyze_sound():
-    # parse JSON received from SQS
-    data = request.get_json()
-    key = str(data["guardianAudio"]["uri:"])
-    # SL call to analyze the audio linked to given key value
-    gevent.joinall([
-        gevent.spawn(service_layer.AnalyzeSound(key, data))
-    ])
-    return """Background worker thread started!"""
+# Set to enable tracebacks on Beanstalk log output
+application.debug = True
 
-@application.route('/updateSoundProfile', methods=['POST'])
-def update_sound_profile():
-    # parse JSON received to get filename/key
-    data = request.get_json()
-    # SL call to analyze the audio linked to given key value
-    gevent.joinall([gevent.spawn()])
-    return "Update the sounds from ML."
+@application.route('/')
+def hello_world():
+    return "Hello, world!"
+
+#routes to call the correct request handlers
+# @application.route('/analyzeSound', methods=['POST'])
+# def analyze_sound():
+#     # parse JSON received from SQS
+#     data = request.get_json()
+#     key = str(data["guardianAudio"]["uri:"])
+#     # SL call to analyze the audio linked to given key value
+#     gevent.joinall([
+#         gevent.spawn(service_layer.AnalyzeSound(key, data))
+#     ])
+#     return """Background worker thread started!"""
+
+# @application.route('/updateSoundProfile', methods=['POST'])
+# def update_sound_profile():
+#     # parse JSON received to get filename/key
+#     data = request.get_json()
+#     # SL call to analyze the audio linked to given key value
+#     gevent.joinall([gevent.spawn()])
+#     return "Update the sounds from ML."
 
 if __name__ == "__main__":
-    application.run()
-
+    application.run(host='0.0.0.0')
--- rfcx-analysis.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/rfcx-analysis')
-
-from app import application
