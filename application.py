ec1c5d525f59ac0bb4862c06d34e22509c1ee390
Kevin Keller <flygeneticist@gmail.com>
2014-12-28 19:27:04 +0100

    Added app functionality back into app.

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 # web server modules
 from flask import Flask, request
-# import gevent
-# import requests
-# import json
+import os
+import gevent
+import requests
+import json
 
 # import RFCx custom modules via service layer
-# from modules import service_layer
+from modules import service_layer
 
 # setup web app instance
 application = Flask(__name__)
@@ -13,29 +14,28 @@ application = Flask(__name__)
 # Set to enable tracebacks on Beanstalk log output
 application.debug = True
 
-@application.route('/analyzeSound')
-def hello_world():
-    return "Hello, world!"
+# routes to call the correct request handlers
+@application.route('/analyzeSound', methods=['POST'])
+def analyze_sound():
+    # parse JSON received from SQS
+    data = request.get_json()
+    key = str(data["guardianAudio"]["uri:"])
+    # SL call to analyze the audio linked to given key value
+    gevent.joinall([
+        gevent.spawn(service_layer.AnalyzeSound(key, data))
+    ])
+    return """Background worker thread started!"""
 
-#routes to call the correct request handlers
-# @application.route('/analyzeSound', methods=['POST'])
-# def analyze_sound():
-#     # parse JSON received from SQS
-#     data = request.get_json()
-#     key = str(data["guardianAudio"]["uri:"])
-#     # SL call to analyze the audio linked to given key value
-#     gevent.joinall([
-#         gevent.spawn(service_layer.AnalyzeSound(key, data))
-#     ])
-#     return """Background worker thread started!"""
-
-# @application.route('/updateSoundProfile', methods=['POST'])
-# def update_sound_profile():
-#     # parse JSON received to get filename/key
-#     data = request.get_json()
-#     # SL call to analyze the audio linked to given key value
-#     gevent.joinall([gevent.spawn()])
-#     return "Update the sounds from ML."
+@application.route('/updateSoundProfile', methods=['POST'])
+def update_sound_profile():
+    # parse JSON received to get filename/key
+    data = request.get_json()
+    # SL call to analyze the audio linked to given key value
+    gevent.joinall([gevent.spawn()])
+    return "Update the sounds from ML."
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    try:
+        os.mkdir('./tmp')
+    finally:
+        application.run(host='0.0.0.0')
