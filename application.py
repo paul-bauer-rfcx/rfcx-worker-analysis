1070bd0b25e48dade2b4cf2c2a368b409832ec91
Kevin Keller <flygeneticist@gmail.com>
2014-12-29 16:28:31 +0100

    Removing possible error code, implement health check url.

--- application.py
+++ application.py
@@ -13,28 +13,31 @@ application = Flask(__name__)
 # Set to enable tracebacks on Beanstalk log output
 application.debug = True
 
-# routes to call the correct request handlers
-@application.route('/analyzeSound', methods=['POST'])
-def analyze_sound():
-    # parse JSON received from SQS
-    data = json.loads(request.data)
-    key = str(data["guardianAudio"]["uri:"])
-    # SL call to analyze the audio linked to given key value
-    # gevent.joinall([
-    #     gevent.spawn(service_layer.AnalyzeSound(key, data))
-    # ])
-    return """Background worker thread started!"""
+@application.route('/healthCheck')
+def check_stuff():
+    return "Server is running"
 
-@application.route('/updateSoundProfile', methods=['POST'])
-def update_sound_profile():
-    # parse JSON received to get filename/key
-    data = request.get_json()
-    # SL call to analyze the audio linked to given key value
-    gevent.joinall([gevent.spawn()])
-    return "Update the sounds from ML."
+# routes to call the correct request handlers
+# @application.route('/analyzeSound', methods=['POST'])
+# def analyze_sound():
+#     # parse JSON received from SQS
+#     data = json.loads(request.data)
+#     key = str(data["guardianAudio"]["uri:"])
+#     # SL call to analyze the audio linked to given key value
+#     # gevent.joinall([
+#     #     gevent.spawn(service_layer.AnalyzeSound(key, data))
+#     # ])
+#     return """Background worker thread started!"""
+
+# @application.route('/updateSoundProfile', methods=['POST'])
+# def update_sound_profile():
+#     # parse JSON received to get filename/key
+#     data = request.get_json()
+#     # SL call to analyze the audio linked to given key value
+#     gevent.joinall([gevent.spawn()])
+#     return "Update the sounds from ML."
 
 if __name__ == "__main__":
     try:
         mkdir('./tmp')
-    finally:
-        application.run(host='0.0.0.0')
+    application.run()
