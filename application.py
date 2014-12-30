ef488eb18ec5e9dbe0a51b60cf4f858f82b0adf6
Kevin Keller <flygeneticist@gmail.com>
2014-12-30 10:10:20 +0100

    Updated JSON parsing to reflect SQS post structure.

--- application.py
+++ application.py
@@ -22,7 +22,7 @@ def check_stuff():
 # @application.route('/analyzeSound', methods=['POST'])
 # def analyze_sound():
 #     # parse JSON received from SQS
-#     data = json.loads(request.data)
+#     data = json.loads(json.loads(request.data)["Message"])
 #     key = str(data["guardianAudio"]["uri:"])
 #     # SL call to analyze the audio linked to given key value
 #     # gevent.joinall([
@@ -33,15 +33,15 @@ def check_stuff():
 @application.route('/updateSoundProfile', methods=['POST'])
 def update_sound_profile():
     # parse JSON received to get filename/key
-    data = json.loads(request.data)
+    data = json.loads(json.loads(request.data)["Message"])["guardianAudio"]["uri:"]
     # SL call to analyze the audio linked to given key value
     # gevent.joinall([
     #     gevent.spawn(service_layer.UpdateSoundProfile(True, True))
     # ])
     # test = service_layer.UpdateSoundProfile(True, True)
-    return "Update the sounds from ML."
+    return """Update the sounds from ML. %s""" % str(data)
 
-def main(argv): 
+def main(argv):
     try:
         mkdir('./tmp')
     except:
