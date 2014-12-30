3683b03a26c785da54dd2d54e563d76b26f72f5e
Kevin Keller <flygeneticist@gmail.com>
2014-12-30 12:07:34 +0100

    Try/Except for base64 and std JSON parsing added. Secondary JSON key/val grabbing commented out to test only a stringified JSON obj was rcvd.

--- application.py
+++ application.py
@@ -23,13 +23,16 @@ def check_stuff():
 @application.route('/analyzeSound', methods=['POST'])
 def analyze_sound():
     try:
-        decoded = request.get_data() # parse JSON received from SQS
-    except (TypeError, ValueError), e:
-        # TO DO: add logging and logic for JSON decoding failures
-        # error_log.exception('Failed to decode JSON from SQS')
-        raise Exception('''***** Failed to decode JSON from SQS *****\n%s'''%(e))
+        decoded = request.get_data()
+    except:
+        try:
+           decoded = base64.b64decode(request.get_data())
+        except (TypeError, ValueError), e:
+            # TO DO: add logging and logic for JSON decoding failures
+            # error_log.exception('Failed to decode JSON from SQS')
+            raise Exception('''***** Failed to decode JSON from SQS *****\n%s'''%(e))
     json_data = json.loads(json.loads(decoded)["Message"])
-    key = str(json_data["guardianAudio"]["uri:"])
+    # key = str(json_data["guardianAudio"]["uri:"])
     # SL call to analyze the audio linked to given key value
     # gevent.joinall([
     #     gevent.spawn(service_layer.AnalyzeSound(key, data))
@@ -40,13 +43,13 @@ def analyze_sound():
 @application.route('/updateSoundProfile', methods=['POST'])
 def update_sound_profile():
     # parse JSON received to get filename/key
-    data = json.loads(json.loads(request.data)["Message"])["guardianAudio"]["uri:"]
+    # data = json.loads(json.loads(request.data)["Message"])["guardianAudio"]["uri:"]
     # SL call to analyze the audio linked to given key value
     # gevent.joinall([
     #     gevent.spawn(service_layer.UpdateSoundProfile(True, True))
     # ])
-    test = service_layer.UpdateSoundProfile(True, True)
-    return """Update the sounds from ML. %s""" % str(data)
+    # test = service_layer.UpdateSoundProfile(True, True)
+    return """Update the sounds from ML."""
 
 def main(argv):
     try:
