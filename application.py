6fe38a61f8826ac029d7c526d7dfb5d0fe0d7770
Kevin Keller <flygeneticist@gmail.com>
2014-12-30 10:57:04 +0100

    JSON parsing from SQS enhanced with, type/overflow attack checks, base64 decoding, and error handling.

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from os import mkdir
 import sys
 import gevent
 import json
+import base64
 
 # import RFCx custom modules via service layer
 from modules import service_layer
@@ -21,14 +22,27 @@ def check_stuff():
 # routes to call the correct request handlers
 @application.route('/analyzeSound', methods=['POST'])
 def analyze_sound():
-    # parse JSON received from SQS
-    data = json.loads(json.loads(request.data)["Message"])
-    key = str(data["guardianAudio"]["uri:"])
-    # SL call to analyze the audio linked to given key value
-    # gevent.joinall([
-    #     gevent.spawn(service_layer.AnalyzeSound(key, data))
-    # ])
-    return """Background worker thread started!"""
+    if request.mime_type == 'application/json' and request.content_length <= 1024**2:
+        # message is JSON and smaller than 1 megabyte (to prevent overload attacks)
+        try:
+            decoded = base64.b64decode(request.get_data()) # parse JSON received from SQS
+        except (ValueError, TypeError), e:
+            # TO DO: add logging and logic for JSON decoding failures
+            # error_log.exception('Failed to decode JSON from SQS')
+            raise Exception('''***** Failed to decode JSON from SQS *****\n%s'''%(e))
+        else:
+            data = json.loads(json.loads(decoded)["Message"])
+            key = str(json_data["guardianAudio"]["uri:"])
+            # SL call to analyze the audio linked to given key value
+            # gevent.joinall([
+            #     gevent.spawn(service_layer.AnalyzeSound(key, data))
+            # ])
+            return """Background worker thread started!"""
+    else:
+        # TO DO: add logging and logic for JSON / SIZE check failures
+        # error_log.exception('SQS data was either not JSON or too large')
+        raise Exception('SQS data was either not JSON or too large')
+
 
 @application.route('/updateSoundProfile', methods=['POST'])
 def update_sound_profile():
