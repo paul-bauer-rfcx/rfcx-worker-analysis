6465b021236fa2147778a477c7bed6f823c09ab7
Kevin Keller <flygeneticist@gmail.com>
2014-12-30 10:14:18 +0100

    AnalyzeSound crippled method turned on with correct JSON. SQS pushed should flow w/o 500 errors.

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ import gevent
 import json
 
 # import RFCx custom modules via service layer
-# from modules import service_layer
+from modules import service_layer
 
 # setup web app instance
 application = Flask(__name__)
@@ -19,16 +19,16 @@ def check_stuff():
     return "Server is running"
 
 # routes to call the correct request handlers
-# @application.route('/analyzeSound', methods=['POST'])
-# def analyze_sound():
-#     # parse JSON received from SQS
-#     data = json.loads(json.loads(request.data)["Message"])
-#     key = str(data["guardianAudio"]["uri:"])
-#     # SL call to analyze the audio linked to given key value
-#     # gevent.joinall([
-#     #     gevent.spawn(service_layer.AnalyzeSound(key, data))
-#     # ])
-#     return """Background worker thread started!"""
+@application.route('/analyzeSound', methods=['POST'])
+def analyze_sound():
+    # parse JSON received from SQS
+    data = json.loads(json.loads(request.data)["Message"])
+    key = str(data["guardianAudio"]["uri:"])
+    # SL call to analyze the audio linked to given key value
+    # gevent.joinall([
+    #     gevent.spawn(service_layer.AnalyzeSound(key, data))
+    # ])
+    return """Background worker thread started!"""
 
 @application.route('/updateSoundProfile', methods=['POST'])
 def update_sound_profile():
@@ -38,7 +38,7 @@ def update_sound_profile():
     # gevent.joinall([
     #     gevent.spawn(service_layer.UpdateSoundProfile(True, True))
     # ])
-    # test = service_layer.UpdateSoundProfile(True, True)
+    test = service_layer.UpdateSoundProfile(True, True)
     return """Update the sounds from ML. %s""" % str(data)
 
 def main(argv):
