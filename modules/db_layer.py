07d91f98850367d38281d923af46504e2e7ea7f4
wylie <joshua.cullick@gmail.com>
2014-12-31 12:49:49 +0200

    introduction to Redis work

--- modules/db_layer.py
+++ modules/db_layer.py
@@ -1,14 +1,38 @@
 '''
 Contains repositories for domain modules 
 ''' 
+import uuid
+import redis
+def c(s):
+	print s
 
 class AnomalyDetectionRepo: 
 	'''
-	Todo: replace the mock by a reddis implementation 
-		  with the same signatures 
+	Now working with Redis,...
 	'''
+	
+
+
 	def __init__(self):
 		self.model = None
+		self.r= redis.StrictRedis(host='localhost', port=6379, db=0)
+		self.baseH= "baseHash"
+		self.r.set('foo', 'bar')
+		bee= self.r.get('foo')
+		c(bee)
+
+	def register_station(self, station):
+		''' registers a new guardian/station into the system'''
+		# lets assume station is simply a string.
+		stationHZ= uuid.uuid4()
+		c(stationHZ)
+		c(station)
+		c(self.r)
+
+		self.r.hset(self.baseH, station, stationHZ)
+		x= self.r.hget(self.baseH, station)
+		c(x)
+
 
 	def get_model(self, station): 
 		''' returns the current model or None, if
@@ -19,3 +43,7 @@ class AnomalyDetectionRepo:
 	def update_model(self, station, model): 
 		# Todo: save model to redis 
 		self.model = model 
+
+x= AnomalyDetectionRepo()
+x.register_station("station1")
+
