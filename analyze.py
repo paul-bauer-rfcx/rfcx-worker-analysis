5dc1270fb59aff308ee635d8f79c5f0736a5e5e0
Kevin Keller <flygeneticist@gmail.com>
2014-12-31 15:13:33 +0100

    Added meta-data args + removed dl function, as local file will be provided now.

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import sys
 import argparse
@@ -6,42 +6,51 @@ import logging
 from modules import service_layer
 import os
 
-def parse_arguments(): 
-	parser = argparse.ArgumentParser(description='Analyze sound file') 
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Analyze sound file')
 	parser.add_argument("-b","--background", help="run process in background (does not work on windows machines)", action="store_true")
 	parser.add_argument("-l","--local", help="use a local file", action="store_true")
-	parser.add_argument("-gid","--guardian_ID", help="ID of the guardian", required=True)
-	parser.add_argument("URL", help="URL to a wave file or path to a local file (see option -l)")
-	
+	parser.add_argument("fp", help="File path to a Wav audio file", required=True)
+	# TO DO: Need an encapsalation option to pass these in as well from Node app.
+	# parser.add_argument('meta', type=file)
+	# TO DO: IF the encapulatation method is NOT given: all flags required=True.
+	# 		 ELSE ignore all manual meta flags
+	parser.add_argument("-gid","--guardian_ID", help="ID of the guardian")
+	parser.add_argument("-aid","--audio_ID", help="ID of the audio file")
+	parser.add_argument("-t","--start_time", help="Local time audio recording started (ex. '2014-12-23T13:46:42.311Z').")
+	parser.add_argument("-dur","--duration_ms", help="ID of the guardian")
+	parser.add_argument("-lat","--latitude", help="ID of the guardian")
+	parser.add_argument("-lon","--longitude", help="ID of the guardian")
+	parser.add_argument("-amb","--ambientTemp", help="ID of the guardian")
+
 	return parser.parse_args()
 
-def setup_logging(): 
+def setup_logging():
 	logger = logging.getLogger("services")
-	logger.addHandler(logging.StreamHandler())  
+	logger.addHandler(logging.StreamHandler())
 	logger.addHandler(logging.FileHandler("logs/services.log"))
-	# Todo: set logging level via config file / command line  
+	# Todo: set logging level via config file / command line
 	logger.setLevel(level=logging.INFO)
 	return logger
 
 def main():
-	args = parse_arguments() 
-	logger = setup_logging() 
+	args = parse_arguments()
+	logger = setup_logging()
 
-	# download / read sound file 
+	# read sound file from fs passed by sys.argv
 	aa = service_layer.AcquireAudio(logger)
-	download_func = aa.read if args.local else aa.download
-	sound = download_func(args.URL, args.guardian_ID)
-	
-	# fork if background option is set 
-	if args.background and os.name != "nt": 
+	sound = aa.read(args.URL, args.guardian_ID)
+
+	# fork if background option is set
+	if args.background and os.name != "nt":
 		newpid = os.fork()
-		if newpid != 0: 
+		if newpid != 0:
 			# exit parent
 			sys.os._exit(0)
 
 	analyzer = service_layer.AnalyzeSound(logger)
 	analyzer.analyze(sound)
 
-	
+
 if __name__ == "__main__":
 	main()
--- modules/service_layer.py
+++ modules/service_layer.py
@@ -3,40 +3,33 @@ Contains high-level code from which calls to the logic layer can be made.
 '''
 import re
 import logging
-import db_layer 
+import db_layer
 import os
 #import custom RFCx modules from modules folder
 from domain_modules import load_sound
 from domain_modules import spectral_analysis
 from domain_modules import fingerprinting
 from domain_modules import sound_profiling
-from domain_modules import sound_classification 
-from domain_modules import anomaly_detection 
+from domain_modules import sound_classification
+from domain_modules import anomaly_detection
 from domain_modules import alerts
 
 
 # Todo: replace logging level by config file or command line options
 
-class Service(object): 
-    # standard dependencies for services 
-    def __init__(self, logger): 
-        self.logger = logger 
+class Service(object):
+    # standard dependencies for services
+    def __init__(self, logger):
+        self.logger = logger
 
-class AcquireAudio(Service): 
-    def download(self, url, guardian_id): 
-        self.logger.info("Downloading sound file '%s'", url)
-        fs = load_sound.download_file(url)
-        sound = self.read(fs, guardian_id)
-        os.remove(fs)
-        return sound
-
-    def read(self, fs, guardian_id): 
+class AcquireAudio(Service):
+    def read(self, fs, guardian_id):
         self.logger.info("Reading sound file '%s'", fs)
         data, samplerate = load_sound.read_sound(fs)
         return load_sound.Sound(data,samplerate, guardian_id)
 
 class AnalyzeSound(Service):
-    def __init__(self, logger, spectral_analyzer=spectral_analysis.SpectralAnalysis() ): 
+    def __init__(self, logger, spectral_analyzer=spectral_analysis.SpectralAnalysis() ):
         self.spec_analyzer = spectral_analyzer
         super(AnalyzeSound, self).__init__(logger)
 
@@ -44,27 +37,27 @@ class AnalyzeSound(Service):
         '''Analyze: Processes a wav file into a spectrum, profile it,
         and trigger results.
         '''
-        # basic workflow 
+        # basic workflow
 
-        # (1) spectral analysis 
+        # (1) spectral analysis
         self.logger.info("Performing spectral analyis for guardian '%s' ", sound.guardian_id)
         self.spec_analyzer.add_spectrum(sound)
 
-        # (2) create an audio finger print 
+        # (2) create an audio finger print
         prof_meta = fingerprinting.Fingerprinter(sound).profile
 
-        # (3) classify the sound via known sound sources 
+        # (3) classify the sound via known sound sources
         sound_classification.SoundClassifier(self.logger).classify(prof_meta)
 
-        # (4) use ML to determine whether the sound is an anomaly 
+        # (4) use ML to determine whether the sound is an anomaly
         # Todo: add requirements for anomaly detection, then add these lines
-        repo = db_layer.AnomalyDetectionRepo() 
+        repo = db_layer.AnomalyDetectionRepo()
         anomaly_detection.AnomalyDetector(self.logger, repo).determine_anomaly(prof_meta)
 
-        # (5) 
+        # (5)
         prof_final = sound_profiling.SoundProfiler(prof_meta).profile
 
-        # (6) alert if necessary 
+        # (6) alert if necessary
         alert = alerts.Alert(prof_final)
 
 
