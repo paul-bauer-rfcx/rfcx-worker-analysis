62903d214b4647330153a9b40966ae677a9a40e0
wylie <joshua.cullick@gmail.com>
2015-01-05 12:57:51 +0200

    modded db_layer.py a bit: removed inline testing debris, changed uid var refs to have a 'Z' suffix, and hopefully didn't break anything

--- modules/db_layer.py
+++ modules/db_layer.py
@@ -25,10 +25,10 @@ class AnomalyDetectionRepo:
         stationHZ= uuid.uuid4()
         meanLZ= uuid.uuid4()
         varianceLZ= uuid.uuid4()
-        sumSquareDif = uuid.uuid4()
-        n = uuid.uuid4()
+        sumSquareDifZ = uuid.uuid4()
+        nZ = uuid.uuid4()
         self.r.hset(self.baseH, station, stationHZ)
-        stationH= {'varianceLZ': varianceLZ, 'meanLZ': meanLZ, 'sumSquareDif': sumSquareDif, 'n': n}
+        stationH= {'varianceLZ': varianceLZ, 'meanLZ': meanLZ, 'sumSquareDifZ': sumSquareDifZ, 'nZ': nZ}
         self.r.hmset(stationHZ, stationH)
 
     def toNumpy(self, array): 
@@ -41,28 +41,25 @@ class AnomalyDetectionRepo:
         exists= self.r.hexists(self.baseH, station)
 
         if not exists:
-            return None 
+            return None
 
         # get keys 
         stationHZ= self.r.hget(self.baseH, station)
         stationH= self.r.hgetall(stationHZ)
         meanLZ= stationH['meanLZ']
         varianceLZ= stationH['varianceLZ']
-        sumSquareDif = stationH['sumSquareDif']
-        n = stationH['n']
- 
-        
+        sumSquareDifZ = stationH['sumSquareDifZ']
+        nZ = stationH['nZ']
+
         # model variables 
         m_mean = self.toNumpy( self.r.lrange(meanLZ, 0, -1) )
         m_var = self.toNumpy( self.r.lrange(varianceLZ, 0, -1) )
-        m_sumSquareDif = self.toNumpy( self.r.lrange(sumSquareDif, 0,-1) )
-        m_n = float( self.r.get(n) )
+        m_sumSquareDif = self.toNumpy( self.r.lrange(sumSquareDifZ, 0,-1) )
+        m_n = float( self.r.get(nZ) )
         return anomaly_detection.Gaussian( m_mean, m_var, m_sumSquareDif, m_n) 
       
 
     def update_model(self, station, model):
-        # here we assume station is a string and model looks like:
-        # model= {'meanL': [bunch of floats], 'varianceL' [bunch of floats] }
         exists= self.r.hexists(self.baseH, station)
         if not(exists):
             c("need to register it first")
@@ -71,8 +68,8 @@ class AnomalyDetectionRepo:
         stationH= self.r.hgetall(stationHZ)
         meanLZ= stationH['meanLZ']
         varianceLZ= stationH['varianceLZ']
-        sumSquareDifLZ = stationH['sumSquareDif']
-        nLZ = stationH['n']
+        sumSquareDifLZ = stationH['sumSquareDifZ']
+        nLZ = stationH['nZ']
 
         self.r.delete(meanLZ)
         self.r.delete(varianceLZ)
@@ -83,50 +80,5 @@ class AnomalyDetectionRepo:
         self.r.set(nLZ, model.n)
 
 
-# TESTS INLINE
 
-# x= AnomalyDetectionRepo()
-# station= "station1"
-# x.register_station(station)
-
-# nonexistentStation= "stationNonExiste"
-# val0= x.get_model(nonexistentStation)
-# c("nonexistent: ", val0)
-
-# meanL= []
-# varianceL= []
-# for i in range(0, 500):
-# 	w=random.random()
-# 	u=random.random()
-# 	meanL.append(w)
-# 	varianceL.append(u)
-# #c(meanL)
-# c("length of meanL prior to update", len(meanL))
-# #c(varianceL)
-# stub= {'meanL': meanL, 'varianceL': varianceL}
-
-# anotherNonexistent= "anotherNon"
-# x.update_model(anotherNonexistent, stub)
-# val1= x.get_model(anotherNonexistent)
-# c("len(val1)", len(val1))
-
-# x.update_model(station, stub)
-
-# z= x.get_model(station)
-# #c(z['meanL'])
-# #c(z['varianceL'])
-# c("type of a given value in varianceL ",type(z['varianceL'][4]))
-
-# problem= 'false'
-# for i in range(0, 500):
-# 	if (  (meanL[i] != z['meanL'][i]) or (varianceL[i] != z['varianceL'][i])):
-# 		c('problem')
-# 		problem= 'true'
-
-# if (problem == 'true'):
-# 	c('there was an error in transcription')
-# else:
-# 	c("the arrays outputed are the same as those inputed! ALL GOOD!")
-
-# now test to verify that nonexistent or station without model should return none
 
