de2743fb396d6f2744dd18ec8974cc6a9209438f
paul-bauer-rfcx <paul.bauer.rfcx@gmail.com>
2015-01-06 12:20:23 -0700

    created harmonic-power function in fingerprinting module.  Should aid in detecting

--- modules/domain_modules/fingerprinting.py
+++ modules/domain_modules/fingerprinting.py
@@ -26,9 +26,10 @@ class Profile(object):
     def __init__(self, spectrum):
         self.classification = [] # could have many sounds per audio clip
         self.spectrum = spectrum
-        self.peaks = None
+        #self.peaks = None
         self.guardian_id = spectrum.sound.meta_data.get('guardian_id')
         self.anomaly_prob = 0.0
+        self.harmonic_power = None
 
     def getPeaks2(self, t):
         ''' get peaks based on relative height (ignore harmonics)
@@ -38,14 +39,28 @@ class Profile(object):
         ix[:] = (a>np.percentile(a,95)) & ix
         return self.spectrum.freqs[ix]
 
+    def get_harmonic_power(self):
+        """
+        return a 1D array of strength of harmonic peaks for each time
+        in spectrum.times
+        """
+        if self.harmonic_power is not None:
+            return self.harmonic_power
+        pwrs = np.empty_like(self.spectrum.times)
+        for i,t in enumerate(self.spectrum.times):
+            pwrs[i] = self.getPeaks(t)[2]
+        self.harmonic_power = pwrs
+        return pwrs
+
     def getPeaks(self, t, ct=10):
         '''
         find harmonic peaks in spectrum at given time
         t .. time value (seconds) to sample spectrum
         ct .. number of peaks to return
+        returns list of freqencies where harmonic peaks are
         '''
-        if self.peaks is not None:
-            return self.peaks
+        #if self.peaks is not None:
+        #    return self.peaks
 
         s = self.spectrum
         a = self.spectrum.timeslice(t)
@@ -65,6 +80,12 @@ class Profile(object):
         ix = (base*intvl).astype(int)
         peak_freqs = s.freqs[ix]
         peak_mags = (a[ix]*2+a[ix-1]+a[ix+1])/4.
+
+        overall_mag = np.average(a)
+        peaks_mag = np.average(peak_mags)
+        #20.*np.log10(np.average(10**(peak_mags/20.)))
+
+        harmonic_power = peaks_mag/overall_mag
         self.peaks = peak_freqs
 
-        return self.peaks
+        return self.peaks, peak_mags, harmonic_power
